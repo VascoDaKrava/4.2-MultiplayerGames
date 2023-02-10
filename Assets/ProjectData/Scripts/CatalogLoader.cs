26f0ed0a85535ad987c595553e8e0db6fca93586
VascoDaKrava <kravchuk.vasilii@yandex.ru>
2023-02-10 16:34:19 +0300

    Lesson code. Catalog JSON Code from Lesson to Lobby-scene was added. My catalog in JSON format to project root folder was added.

---
+++ Assets/ProjectData/Scripts/CatalogLoader.cs
@@ -0,0 +1,40 @@
+using PlayFab.ClientModels;
+using PlayFab;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public sealed class CatalogLoader : MonoBehaviour
+{
+
+    private readonly Dictionary<string, CatalogItem> _catalog = new Dictionary<string, CatalogItem>();
+
+    private void Start()
+    {
+        PlayFabClientAPI.GetCatalogItems(
+            new GetCatalogItemsRequest(),
+            OnGetCatalogSuccess,
+            OnFailure);
+    }
+
+    private void OnFailure(PlayFabError error)
+    {
+        var errorMessage = error.GenerateErrorReport();
+        Debug.LogError($"Something went wrong: {errorMessage}");
+    }
+
+    private void OnGetCatalogSuccess(GetCatalogItemsResult result)
+    {
+        HandleCatalog(result.Catalog);
+        Debug.Log($"Catalog was loaded successfully!");
+    }
+
+    private void HandleCatalog(List<CatalogItem> catalog)
+    {
+        foreach (var item in catalog)
+        {
+            _catalog.Add(item.ItemId, item);
+            Debug.Log($"Catalog item {item.ItemId} was added successfully!");
+        }
+    }
+}
